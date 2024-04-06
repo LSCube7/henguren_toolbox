2886e5abfb2994faad16dfabd0f15d0becdbf55e
小方盒LSCube <ck20130914@outlook.com>
2024-04-07 00:25:45 +0800

    update cache

--- share/footer.js
+++ share/footer.js
@@ -5,3 +5,11 @@ document.addEventListener("DOMContentLoaded", function () {
       document.querySelector("footer").innerHTML = data;
     });
 });
+
+window.addEventListener('load', function() {
+  if ('serviceWorker' in navigator) {
+    // 发送消息给 Service Worker 请求重新加载页面
+    navigator.serviceWorker.controller.postMessage({ type: 'RELOAD_PAGE' });
+  }
+});
+
